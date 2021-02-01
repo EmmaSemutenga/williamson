e73e1deb0bb2ed68be8da89c1f261d876c19d1fc
Emmanuel Semutenga <emzsema@gmail.com>
2021-02-01 12:56:47 +0300

    added othe files

---
+++ firm/__init__.py
@@ -0,0 +1,26 @@
+from sqlalchemy import desc
+from flask import Flask  
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+ENV = 'prod'
+if ENV =='prod':
+    app.debug =True
+    app.config['SECRET_KEY']='SECRET_KEY'
+    app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:ndb123@localhost/henry'
+    
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI']=''
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+db= SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'index'
+login_manager.login_message_category = 'info'
+
+from firm import routes
